a5a1e376596f68d7c78bf22ef4741a0406a54226
araten10 <141581742+araten10@users.noreply.github.com>
2024-11-10 14:03:48 -0500

    preparing settings.py for full corruption

--- edgeware/src/settings.py
+++ edgeware/src/settings.py
@@ -38,6 +38,7 @@ config_blacklist = [
     "rotateWallpaper",
     "replace",  # replace images
     "replaceThresh",
+    "avoidList",  # avoid list for replace images. also works on fill drive but due to filepath the only thing you can do with this is make it blank
     "start_on_logon",
     "showDiscord",  # show discord status, technically not PC dangerous but socially dangerous
     "timerMode",  # locks out panic until certain time has passed
@@ -58,7 +59,6 @@ config_dangerous = [
     "fill",  # fill drive
     "fill_delay",
     "maxFillThreads",
-    "avoidList",  # avoid list for replace images, also might not work considering pc differences
     "panicDisabled",  # disables panic in hotkey/system tray, can still be run via panic.pyw
     "webPopup",  # opens up web popup on popup close, this one could be cut from this list as it's not listed as dangerous in config but could lead to bad performance
 ]
@@ -104,124 +104,127 @@ def load_default_config() -> dict:
 
 class Settings:
     def __init__(self):
-        config = load_config()
+        self.config = load_config()
+        self.load_settings()
 
+
+    def load_settings(self):
         # Impacts other settings
-        lowkey_mode = bool(config["lkToggle"])
-        mitosis_mode = bool(config["mitosisMode"]) or lowkey_mode
+        lowkey_mode = bool(self.config["lkToggle"])
+        mitosis_mode = bool(self.config["mitosisMode"]) or lowkey_mode
 
         # General
-        self.theme = config["themeType"]
-        self.startup_splash = bool(config["showLoadingFlair"])
-        self.desktop_icons = bool(config["desktopIcons"])
-        self.panic_key = config["panicButton"]
+        self.theme = self.config["themeType"]
+        self.startup_splash = bool(self.config["showLoadingFlair"])
+        self.desktop_icons = bool(self.config["desktopIcons"])
+        self.panic_key = self.config["panicButton"]
 
         # Booru downloader
-        self.booru_download = bool(config["downloadEnabled"])
-        self.booru_name = config["booruName"]
-        # self.min_score = int(config["booruMinScore"])  # TODO: Can this be used with gallery-dl?
-        self.booru_tags = config["tagList"].replace(">", "+")  # TODO: Store in a better way
+        self.booru_download = bool(self.config["downloadEnabled"])
+        self.booru_name = self.config["booruName"]
+        # self.min_score = int(self.config["booruMinScore"])  # TODO: Can this be used with gallery-dl?
+        self.booru_tags = self.config["tagList"].replace(">", "+")  # TODO: Store in a better way
         self.download_path = Data.DOWNLOAD / f"{self.booru_name}-{self.booru_tags}"
 
         # Popups
-        self.delay = int(config["delay"])  # Milliseconds
-        self.image_chance = int(config["popupMod"]) if not mitosis_mode else 0  # 0 to 100
-        self.opacity = int(config["lkScaling"]) / 100  # Float between 0 and 1
-        self.timeout_enabled = bool(config["timeoutPopups"]) or lowkey_mode
-        self.timeout = int(config["popupTimeout"]) * 1000 if not lowkey_mode else self.delay  # Milliseconds
-        self.buttonless = bool(config["buttonless"])
-        self.single_mode = bool(config["singleMode"])
+        self.delay = int(self.config["delay"])  # Milliseconds
+        self.image_chance = int(self.config["popupMod"]) if not mitosis_mode else 0  # 0 to 100
+        self.opacity = int(self.config["lkScaling"]) / 100  # Float between 0 and 1
+        self.timeout_enabled = bool(self.config["timeoutPopups"]) or lowkey_mode
+        self.timeout = int(self.config["popupTimeout"]) * 1000 if not lowkey_mode else self.delay  # Milliseconds
+        self.buttonless = bool(self.config["buttonless"])
+        self.single_mode = bool(self.config["singleMode"])
 
         # Overlays
-        self.denial_chance = int(config["denialChance"]) if bool(config["denialMode"]) else 0  # 0 to 100
-        self.subliminal_chance = int(config["subliminalsChance"]) if bool(config["popupSubliminals"]) else 0  # 0 to 100
-        self.max_subliminals = int(config["maxSubliminals"])
-        self.subliminal_opacity = int(config["subliminalsAlpha"]) / 100  # Float between 0 and 1
+        self.denial_chance = int(self.config["denialChance"]) if bool(self.config["denialMode"]) else 0  # 0 to 100
+        self.subliminal_chance = int(self.config["subliminalsChance"]) if bool(self.config["popupSubliminals"]) else 0  # 0 to 100
+        self.max_subliminals = int(self.config["maxSubliminals"])
+        self.subliminal_opacity = int(self.config["subliminalsAlpha"]) / 100  # Float between 0 and 1
 
         # Web
-        self.web_chance = int(config["webMod"])  # 0 to 100
-        self.web_on_popup_close = bool(config["webPopup"]) and not lowkey_mode
+        self.web_chance = int(self.config["webMod"])  # 0 to 100
+        self.web_on_popup_close = bool(self.config["webPopup"]) and not lowkey_mode
 
         # Prompt
-        self.prompt_chance = int(config["promptMod"])  # 0 to 1000
-        self.prompt_max_mistakes = int(config["promptMistakes"])
+        self.prompt_chance = int(self.config["promptMod"])  # 0 to 1000
+        self.prompt_max_mistakes = int(self.config["promptMistakes"])
 
         # Audio
-        self.audio_chance = int(config["audioMod"])  # 0 to 100
-        self.max_audio = int(config["maxAudio"])
+        self.audio_chance = int(self.config["audioMod"])  # 0 to 100
+        self.max_audio = int(self.config["maxAudio"])
 
         # Video
-        self.video_chance = int(config["vidMod"]) if not mitosis_mode else 0  # 0 to 100
-        self.video_volume = int(config["videoVolume"])  # 0 to 100
-        self.max_video = int(config["maxVideos"]) if bool(config["maxVideoBool"]) else float("inf")
-        self.vlc_mode = bool(config["vlcMode"])
+        self.video_chance = int(self.config["vidMod"]) if not mitosis_mode else 0  # 0 to 100
+        self.video_volume = int(self.config["videoVolume"])  # 0 to 100
+        self.max_video = int(self.config["maxVideos"]) if bool(self.config["maxVideoBool"]) else float("inf")
+        self.vlc_mode = bool(self.config["vlcMode"])
         try:
             vlc.libvlc_hex_version()  # Check if VLC is installed
         except NameError:
             self.vlc_mode = False
 
         # Captions
-        self.captions_in_popups = bool(config["showCaptions"])
-        self.filename_caption_moods = bool(config["captionFilename"])
-        self.multi_click_popups = bool(config["multiClick"])
-        self.subliminal_caption_mood = bool(config["capPopMood"])
-        self.subliminal_message_popup_chance = int(config["capPopChance"])  # 0 to 100
-        self.subliminal_message_popup_opacity = int(config["capPopOpacity"]) / 100  # Float between 0 and 1
-        self.subliminal_message_popup_timeout = int(config["capPopTimer"])  # Milliseconds
-        self.notification_mood = bool(config["notificationMood"])
-        self.notification_chance = int(config["notificationChance"])  # 0 to 100
-        self.notification_image_chance = int(config["notificationImageChance"])  # 0 to 100
+        self.captions_in_popups = bool(self.config["showCaptions"])
+        self.filename_caption_moods = bool(self.config["captionFilename"])
+        self.multi_click_popups = bool(self.config["multiClick"])
+        self.subliminal_caption_mood = bool(self.config["capPopMood"])
+        self.subliminal_message_popup_chance = int(self.config["capPopChance"])  # 0 to 100
+        self.subliminal_message_popup_opacity = int(self.config["capPopOpacity"]) / 100  # Float between 0 and 1
+        self.subliminal_message_popup_timeout = int(self.config["capPopTimer"])  # Milliseconds
+        self.notification_mood = bool(self.config["notificationMood"])
+        self.notification_chance = int(self.config["notificationChance"])  # 0 to 100
+        self.notification_image_chance = int(self.config["notificationImageChance"])  # 0 to 100
 
         # Wallpaper
-        self.rotate_wallpaper = bool(config["rotateWallpaper"])
-        self.wallpaper_timer = int(config["wallpaperTimer"]) * 1000  # Milliseconds
-        self.wallpaper_variance = int(config["wallpaperVariance"]) * 1000  # Milliseconds
-        self.wallpapers = list(ast.literal_eval(config["wallpaperDat"]).values())  # TODO: Can fail, store in a better way
+        self.rotate_wallpaper = bool(self.config["rotateWallpaper"])
+        self.wallpaper_timer = int(self.config["wallpaperTimer"]) * 1000  # Milliseconds
+        self.wallpaper_variance = int(self.config["wallpaperVariance"]) * 1000  # Milliseconds
+        self.wallpapers = list(ast.literal_eval(self.config["wallpaperDat"]).values())  # TODO: Can fail, store in a better way
 
         # Dangerous
-        self.drive_avoid_list = config["avoidList"].split(">")  # TODO: Store in a better way
-        self.fill_drive = bool(config["fill"])
-        self.drive_path = config["drivePath"]
-        self.fill_delay = int(config["fill_delay"]) * 10  # Milliseconds
-        self.replace_images = bool(config["replace"])
-        self.replace_threshold = int(config["replaceThresh"])
-        self.panic_disabled = bool(config["panicDisabled"])
-        self.show_on_discord = bool(config["showDiscord"])
+        self.drive_avoid_list = self.config["avoidList"].split(">")  # TODO: Store in a better way
+        self.fill_drive = bool(self.config["fill"])
+        self.drive_path = self.config["drivePath"]
+        self.fill_delay = int(self.config["fill_delay"]) * 10  # Milliseconds
+        self.replace_images = bool(self.config["replace"])
+        self.replace_threshold = int(self.config["replaceThresh"])
+        self.panic_disabled = bool(self.config["panicDisabled"])
+        self.show_on_discord = bool(self.config["showDiscord"])
 
         # Basic modes
         self.lowkey_mode = lowkey_mode
-        self.lowkey_corner = int(config["lkCorner"])
-        self.moving_chance = int(config["movingChance"])  # 0 to 100
-        self.moving_speed = int(config["movingSpeed"])
-        self.moving_random = bool(config["movingRandom"])
+        self.lowkey_corner = int(self.config["lkCorner"])
+        self.moving_chance = int(self.config["movingChance"])  # 0 to 100
+        self.moving_speed = int(self.config["movingSpeed"])
+        self.moving_random = bool(self.config["movingRandom"])
 
         # Dangerous modes
-        self.timer_mode = bool(config["timerMode"])
-        self.timer_time = int(config["timerSetupTime"]) * 60 * 1000  # Milliseconds
-        self.timer_password = config["safeword"]
+        self.timer_mode = bool(self.config["timerMode"])
+        self.timer_time = int(self.config["timerSetupTime"]) * 60 * 1000  # Milliseconds
+        self.timer_password = self.config["safeword"]
         self.mitosis_mode = mitosis_mode
-        self.mitosis_strength = int(config["mitosisStrength"]) if not self.lowkey_mode else 1
+        self.mitosis_strength = int(self.config["mitosisStrength"]) if not self.lowkey_mode else 1
 
         # Hibernate mode
-        self.hibernate_mode = bool(config["hibernateMode"])
-        self.hibernate_fix_wallpaper = bool(config["fixWallpaper"]) and self.hibernate_mode
-        self.hibernate_type = config["hibernateType"]
-        self.hibernate_delay_min = int(config["hibernateMin"]) * 1000  # Milliseconds
-        self.hibernate_delay_max = int(config["hibernateMax"]) * 1000  # Milliseconds
-        self.hibernate_activity = int(config["wakeupActivity"])
-        self.hibernate_activity_length = int(config["hibernateLength"]) * 1000  # Milliseconds
+        self.hibernate_mode = bool(self.config["hibernateMode"])
+        self.hibernate_fix_wallpaper = bool(self.config["fixWallpaper"]) and self.hibernate_mode
+        self.hibernate_type = self.config["hibernateType"]
+        self.hibernate_delay_min = int(self.config["hibernateMin"]) * 1000  # Milliseconds
+        self.hibernate_delay_max = int(self.config["hibernateMax"]) * 1000  # Milliseconds
+        self.hibernate_activity = int(self.config["wakeupActivity"])
+        self.hibernate_activity_length = int(self.config["hibernateLength"]) * 1000  # Milliseconds
         # TODO: Pump-scare audio
-        # self.pump_scare_offset = int(config["pumpScareOffset"])  # Seconds
+        # self.pump_scare_offset = int(self.config["pumpScareOffset"])  # Seconds
 
         # Corruption mode
-        self.corruption_mode = bool(config["corruptionMode"])
-        # self.corruption_full = bool(config["corruptionFullPerm"])
-        # self.corruption_fade = config["corruptionFadeType"]
-        self.corruption_trigger = config["corruptionTrigger"]
-        self.corruption_time = int(config["corruptionTime"]) * 1000  # Milliseconds
-        self.corruption_popups = int(config["corruptionPopups"])
-        self.corruption_launches = int(config["corruptionLaunches"])
-        self.corruption_wallpaper = not bool(config["corruptionWallpaperCycle"])
-        # self.corruption_themes = not bool(config["corruptionThemeCycle"])
-        self.corruption_purity = bool(config["corruptionPurityMode"])
-        self.corruption_dev_mode = bool(config["corruptionDevMode"])
+        self.corruption_mode = bool(self.config["corruptionMode"])
+        # self.corruption_full = bool(self.config["corruptionFullPerm"])
+        # self.corruption_fade = self.config["corruptionFadeType"]
+        self.corruption_trigger = self.config["corruptionTrigger"]
+        self.corruption_time = int(self.config["corruptionTime"]) * 1000  # Milliseconds
+        self.corruption_popups = int(self.config["corruptionPopups"])
+        self.corruption_launches = int(self.config["corruptionLaunches"])
+        self.corruption_wallpaper = not bool(self.config["corruptionWallpaperCycle"])
+        # self.corruption_themes = not bool(self.config["corruptionThemeCycle"])
+        self.corruption_purity = bool(self.config["corruptionPurityMode"])
+        self.corruption_dev_mode = bool(self.config["corruptionDevMode"])
